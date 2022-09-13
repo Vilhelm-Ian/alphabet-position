f40d25fd7fe60bcf708fc39eadf4afb4ec58ef28
Jovan <mariothrowsfieball@gmail.com>
2022-09-13 13:19:34 +0200

    fixed letter being -1 of the actual position

--- cli.js
+++ cli.js
@@ -5,4 +5,4 @@ let letter = process.argv[2]?.toLowerCase();
 let alphabet = "abcdefghijklmnopqrstuvwxyz";
 let index = alphabet.indexOf(letter);
 if (index == -1) console.log("letter not in alphabet");
-else console.log(index);
+else console.log(index + 1);
